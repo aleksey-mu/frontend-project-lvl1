8c8fb687dc6fcc0de43d123772cda82e20512c1a
aleksey_mu <pacuklex@mail.ru>
2020-01-17 22:56:54 +0300

    first game Even

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ install:
 	npm install
 
 start:
-	npx babel-node src/brain-games.js
+	npx babel-node src/brain-even.js
 
 publish:
 	npm publish --dry-run
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import readlineSync from 'readline-sync';
 
-
-const greetings = () => {
-  console.log('Welcome to the Brain Games!\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const randomNum = () => {
+  const min = 1;
+  const max = 101;
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-export default { greetings };
+const question = (name) => {
+  const number = randomNum();
+  let correctAnswer = null;
+  // eslint-disable-next-line no-unused-expressions
+  number % 2 === 0 ? correctAnswer = 'yes' : correctAnswer = 'no';
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
+
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return false;
+  }
+  console.log(`\`${answer}\` is wrong answer ;(. Correct answer was \`${correctAnswer}\`.\nLet's try again, ${name}`);
+  return true;
+};
+
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".\n');
+
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+
+  for (let i = 1; i <= 3; i += 1) {
+    const exitStatus = question(name);
+    if (exitStatus === true) {
+      break;
+    }
+  }
+};
+
+export default evenGame;
